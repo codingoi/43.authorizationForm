10015e6d6c42d5935533ab4f09ff338be6393e62
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-09-02 15:39:40 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+//Components
+import Contact from './Components/Contact/Contact.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
